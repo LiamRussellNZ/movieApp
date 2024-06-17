2d6a01e2d1a58c32c281e012e3bc95bf80226227
Liam Russell <liam.russell@assurity.co.nz>
2024-06-17 21:21:04 +1200

    Swapped out Postgres for MySQL, added a manifest for cf

--- backend/db.ts
+++ backend/db.ts
@@ -1,7 +1,19 @@
 import { Sequelize } from "sequelize";
-import { dbUser, dbPass, dbHost } from "./config";
+//import { dbUser, dbPass, dbHost, dbName } from "./config";
+import dotenv from "dotenv";
 
-const connectionString = `postgres://${dbUser}:${dbPass}@${dbHost}/movies`;
-const sequelize = new Sequelize(connectionString);
+dotenv.config();
+
+const dbName = process.env.DB_NAME || "default";
+const dbUser = process.env.DB_USER || "";
+const dbPass = process.env.DB_PASS || "";
+const dbHost = process.env.DB_HOST || "";
+const dbPort = process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306;
+
+const sequelize = new Sequelize(dbName, dbUser, dbPass, {
+  host: dbHost,
+  port: dbPort,
+  dialect: "mysql",
+});
 
 export default sequelize;
