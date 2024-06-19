a5738a1a7cc858dec0e38eb639c93ac5b4b08542
Liam Russell <liam.russell@assurity.co.nz>
2024-06-19 16:01:40 +1200

    Modified connection details. Removed Postgres dep

--- backend/db.ts
+++ backend/db.ts
@@ -5,22 +5,23 @@ import process from "process";
 
 dotenv.config();
 
-const VCAP_SERVICES = JSON.parse(process.env.VCAP_SERVICES || "")
-const { database, username, password, hostname, port } = VCAP_SERVICES["csb-aws-aurora-mysql"][0].credentials
+//const VCAP_SERVICES = JSON.parse(process.env.VCAP_SERVICES || "");
+//const { database, username, password, hostname, port } =
+//  VCAP_SERVICES["csb-aws-aurora-mysql"][0].credentials;
 
-if (!VCAP_SERVICES) {
-  console.error("MySQL Database is not available")
-}
+//if (!VCAP_SERVICES) {
+//  console.error("MySQL Database is not available");
+//}
 
-// const dbName = process.env.DB_NAME || "movies";
-// const dbUser = process.env.DB_USER || "root";
-// const dbPass = process.env.DB_PASS || "123qweasd!A";
-// const dbHost = process.env.DB_HOST || "localhost";
-// const dbPort = process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3307; // FIXME
+const dbName = process.env.DB_NAME || "";
+const dbUser = process.env.DB_USER || "";
+const dbPass = process.env.DB_PASS || "";
+const dbHost = process.env.DB_HOST || "";
+const dbPort = process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306;
 
-const sequelize = new Sequelize(database, username, password, {
-  host: hostname,
-  port: port,
+const sequelize = new Sequelize(dbName, dbUser, dbPass, {
+  host: dbHost,
+  port: dbPort,
   dialect: "mysql",
 });
 
